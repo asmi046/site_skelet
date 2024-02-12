ce14c69e717b20cda55a0b02617f318175d247fc
asmi046 <asmi046@gmail.com>
2024-02-12 12:18:53 +0300

    validated

--- src/Http/Controllers/Consultation/SenderConsultController.php
+++ src/Http/Controllers/Consultation/SenderConsultController.php
@@ -15,7 +15,7 @@ use App\Http\Requests\Consultation\ConsultFormRequest;
 class SenderConsultController extends Controller
 {
     public function send_consultation(ConsultFormRequest $request, TelegramSendAction $tgsender) {
-        $data = $request->validate();
+        $data = $request->validated();
 
 
         $tmp = $tgsender->handle("<b>Консультация</b>\n\rИмя: ".$data['name']."\n\rТелефон: ".$data['phone']."\n\rКомментарий: ".$data['message']);
