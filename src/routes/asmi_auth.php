578f02e35191d6e7453b05ad347597bd5ef09eae
asmi046 <asmi046@gmail.com>
2024-02-24 23:34:32 +0300

    Auth

--- src/routes/asmi_auth.php
+++ src/routes/asmi_auth.php
@@ -8,12 +8,12 @@
     });
 
     Route::middleware('guest')->group(function () {
-        Route::get('/login', [AuthController::class, "show_login_login"])->name("login");
+        Route::get('/login', [AuthController::class, "show_login_form"])->name("login");
         Route::post('/login_do', [AuthController::class, "login"])->name("login_do");
 
         Route::get('/password-recovery', [AuthController::class, "show_passrec_page"])->name("passrec");
         Route::post('/pass_rec_do', [AuthController::class, "pass_req"])->name("pass_rec_do");
 
         Route::post('/register_do', [AuthController::class, "register"])->name("register_do");
-        Route::get('/register', [AuthController::class, "show_register_page"])->name("register");
+        Route::get('/register', [AuthController::class, "show_register_form"])->name("register");
     });
